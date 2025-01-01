322a4e2b8207b11a955f8af97c3b95f99d9ea86d
IIRoan <github.com.q6c0y@lunary.roan.zip>
2025-01-01 19:55:44 +0100

    imagetest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     description: "Apex Legends Map Rotation Timer",
     images: [
       {
-        url: "https://apex.roan.dev/screenshot.png",
+        url: "https://opengraph.b-cdn.net/production/images/e7519009-5d19-4426-936b-e87539c42a72.png?token=aGuWr4_mlnjf8P667XyW1q4Y4v7fyI99fUxBYshfDDY&height=617&width=1200&expires=33271757661",
         width: 1200,
         height: 630,
         alt: "Apex Rotation Preview",
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Apex Rotation",
     description: "Apex Legends Map Rotation Timer",
-    images: ["https://apex.roan.dev/screenshot.png"],
+    images: ["https://opengraph.b-cdn.net/production/images/e7519009-5d19-4426-936b-e87539c42a72.png?token=aGuWr4_mlnjf8P667XyW1q4Y4v7fyI99fUxBYshfDDY&height=617&width=1200&expires=33271757661"],
   },
 };
 
